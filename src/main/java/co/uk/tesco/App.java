78e8fa3fb6147355478390650c704c16222260ea
john <speaktojohn101@yahoo.com>
2021-05-27 23:25:18 +0100

    creating new framework

---
+++ src/main/java/co/uk/tesco/App.java
@@ -0,0 +1,13 @@
+package co.uk.tesco;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
